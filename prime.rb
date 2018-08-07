4b42ea85631b847f5172387f01df4d2100f8ca5a
Patricia Carter <patricia4194@gmail.com>
2018-08-07 16:46:20 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 def prime?(int)
   if int > -2 && int < 2 
     return false
-  elsif int == 2 || int = -2
+  elsif int == 2 || int == -2
     return true 
   elsif int < 0
     array = (-2...int).to_a
