88a205fd03326e2fb1fccb67712263f151b16277
Patricia Carter <patricia4194@gmail.com>
2018-08-07 16:42:53 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,11 @@
 def prime?(int)
   if int < 0
-    
+    array = (-2...int).to_a
+    array.each do |i|
+      if array.length % i == 0 
+        return false 
+      else 
+        return true 
   if int == 2 
     return true 
   else 
