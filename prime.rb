d432ea21e45bec8aa6402caa1fe09d2e1668274d
Patricia Carter <patricia4194@gmail.com>
2018-08-07 16:37:18 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,13 @@
 def prime?(int)
-  array = (2...int).to_a
-  array.each do |i|
-    if array.length % i == 0 
-      return false 
-    else 
-      return true 
+  if int == 2 
+    return true 
+  else 
+    array = (2...int).to_a
+    array.each do |i|
+      if array.length % i == 0 
+        return false 
+      else 
+        return true 
+      end
     end
-  end
 end 
