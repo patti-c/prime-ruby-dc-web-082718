466cd8f13806acd5f957de44e2749f90fdb988ba
Patricia Carter <patricia4194@gmail.com>
2018-08-07 16:49:51 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,6 +2,7 @@ def prime?(int)
   if int > -2 && int < 2 
     return false
   elsif int == 2 || int == -2
+    puts "The numbers 2 and -2 are prime."
     return true 
   elsif int < 0
     array = (-2...int).to_a
