27290e8ac889d1bb21df5fd14a7eeece346555ed
Patricia Carter <patricia4194@gmail.com>
2018-08-07 16:44:04 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,14 @@
 def prime?(int)
-  if int < 0
+  if int > -2 && int < 2 
+    return false
+  elsif int == 2 || int = -2
+  elsif int < 0
     array = (-2...int).to_a
     array.each do |i|
       if array.length % i == 0 
         return false 
       else 
         return true 
-  elsif int == 2 || int = -2
     return true 
   else 
     array = (2...int).to_a
